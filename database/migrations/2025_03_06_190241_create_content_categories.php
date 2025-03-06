dfe93cf6b9f5047b4980eaffe226d7676b400b02
Garcia <carlos.garcia.diaz2000@hotmail.com>
2025-03-06 13:08:44 -0600

    migracion content categories realizada

---
+++ database/migrations/2025_03_06_190241_create_content_categories.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('temas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('temas');
+        Schema::dropIfExists('categorias');
+    }
+};
