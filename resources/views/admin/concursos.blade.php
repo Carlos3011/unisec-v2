2845da8c106b91d5f2fedd24031da7f892f996cc
Garcia <carlos.garcia.diaz2000@hotmail.com>
2025-06-13 07:20:56 -0600

    refactor(views): remove unused admin view files
    Clean up admin views that were no longer being used in the application

--- resources/views/admin/concursos.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.admin')
-
-@section('titulo', 'Panel de Concursos')
-
-@section('contenido')
-    <h1 class="text-white">Panel de Administrador</h1>
-    <p class="text-white">Bienvenido a Concursos.</p>
-@endsection
--- resources/views/admin/congreso-eventos.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.admin')
-
-@section('titulo', 'Panel de Congreso')
-
-@section('contenido')
-    <h1 class="text-white">Panel de Administrador</h1>
-    <p class="text-white">Bienvenido a Congreso y Eventos.</p>
-@endsection
