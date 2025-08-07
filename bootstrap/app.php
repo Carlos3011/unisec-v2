0ec9ad2e546c1f20f9a6e6b0be768251d9694c5b
Fiona Danaé <fionareynaga@hotmail.com>
2025-08-06 21:50:22 -0600

    eliminacion polimorfismo en webpay

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,8 @@
-<?php
+z<?php
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
 use App\Http\Middleware\RoleMiddleware;
 
 
